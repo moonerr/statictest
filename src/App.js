1ac2054b6d086cf8c0777d8885801a9797cb1626
Eve Wang <EWang@rpmglobal.com>
2022-06-27 16:26:24 +1000

    add API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await (await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
+
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>
+    Hello {value}
+    {data}
+  </div>;
 }
 
 export default App;
